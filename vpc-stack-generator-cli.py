6352dec1c0df74bab7e170862969177759c1e035
marcus16-kang <marcus16-kang@outlook.com>
2022-10-26 21:46:23 +0900

    Add yaml creator.

--- vpc-stack-generator-cli.py
+++ vpc-stack-generator-cli.py
@@ -182,7 +182,7 @@ class PrintTable:
         self.table.clear()
         self.table.title = 'Internet Gateway'
         self.table.field_names = ['Name']
-        self.table.add_row(igw)
+        self.table.add_row([igw])
 
         print(self.table)
 
@@ -221,23 +221,51 @@ class PrintTable:
 
 class CreateYAML:
     resources = {}
+    region = None
     public_subnet_name = []
     private_subnet_name = []
     protected_subnet_name = []
+    private_rtb_name = []
+
+    def __init__(
+            self,
+            region,
+            vpc,
+            public_subnet=None,
+            private_subnet=None,
+            protected_subnet=None,
+            k8s_tags=False,
+            igw=None,
+            public_rtb=None,
+            private_rtb=None,
+            protected_rtb=None,
+            s3_gateway_ep=None
+    ):
+        self.region = region
+        self.create_vpc(vpc=vpc)
+        self.create_subnets(
+            public_subnet=public_subnet,
+            private_subnet=private_subnet,
+            protected_subnet=protected_subnet,
+            set_k8s_tags=k8s_tags
+        )
+        self.create_igw(igw=igw)
+        self.create_route_tables(public_rtb=public_rtb, private_rtb=private_rtb, protected_rtb=protected_rtb)
+        self.create_s3_ep(s3_gateway_ep=s3_gateway_ep)
 
     def create_vpc(self, vpc):
         self.resources['VPC'] = {
-            'ype': 'AWS::EC2::VPC',
+            'Type': 'AWS::EC2::VPC',
             'Properties': {
                 'CidrBlock': vpc['cidr'],
                 'EnableDnsHostnames': True,
                 'EnableDnsSupport': True,
                 'InstanceTenancy': 'default',
-                'Tags': [{'Name': vpc['name']}]
+                'Tags': [{'Key': 'Name', 'Value': vpc['name']}]
             }
         }
 
-    def create_subnets(self, public_subnet=None, private_subnet=None, protected_subnet=None):
+    def create_subnets(self, public_subnet=None, private_subnet=None, protected_subnet=None, set_k8s_tags=False):
         if public_subnet:
             for i, subnet in enumerate(public_subnet):
                 self.resources['PublicSubnet' + str(i)] = {
@@ -246,13 +274,18 @@ class CreateYAML:
                         'AvailabilityZone': subnet['az'],
                         'CidrBlock': subnet['cidr'],
                         'MapPublicIpOnLaunch': True,
-                        'Tags': [{'Name': subnet['name']}],
+                        'Tags': [{'Key': 'Name', 'Value': subnet['name']}],
                         'VpcId': {
                             'Ref': 'VPC'
                         }
                     }
                 }
                 self.public_subnet_name.append({'cloudformation': 'PublicSubnet' + str(i), 'name': subnet['name']})
+
+                if set_k8s_tags:
+                    self.resources['PublicSubnet' + str(i)]['Properties']['Tags'].append(
+                        {'Key': 'kubernetes.io/role/elb', 'Value': '1'}
+                    )
 
         if private_subnet:
             for i, subnet in enumerate(private_subnet):
@@ -261,14 +294,19 @@ class CreateYAML:
                     'Properties': {
                         'AvailabilityZone': subnet['az'],
                         'CidrBlock': subnet['cidr'],
-                        'MapPublicIpOnLaunch': True,
-                        'Tags': [{'Name': subnet['name']}],
+                        'MapPublicIpOnLaunch': False,
+                        'Tags': [{'Key': 'Name', 'Value': subnet['name']}],
                         'VpcId': {
                             'Ref': 'VPC'
                         }
                     }
                 }
                 self.private_subnet_name.append({'cloudformation': 'PrivateSubnet' + str(i), 'name': subnet['name']})
+
+                if set_k8s_tags:
+                    self.resources['PrivateSubnet' + str(i)]['Properties']['Tags'].append(
+                        {'Key': 'kubernetes.io/role/internal-elb', 'Value': '1'}
+                    )
 
         if protected_subnet:
             for i, subnet in enumerate(protected_subnet):
@@ -277,8 +315,8 @@ class CreateYAML:
                     'Properties': {
                         'AvailabilityZone': subnet['az'],
                         'CidrBlock': subnet['cidr'],
-                        'MapPublicIpOnLaunch': True,
-                        'Tags': [{'Name': subnet['name']}],
+                        'MapPublicIpOnLaunch': False,
+                        'Tags': [{'Key': 'Name', 'Value': subnet['name']}],
                         'VpcId': {
                             'Ref': 'VPC'
                         }
@@ -305,18 +343,33 @@ class CreateYAML:
                 }
             }
         }
+        self.resources['PublicRouteTableRouteIGW'] = {
+            'Type': 'AWS::EC2::Route',
+            'Properties': {
+                'DestinationCidrBlock': '0.0.0.0/0',
+                'GatewayId': {
+                    'Ref': 'IGW'
+                },
+                'RouteTableId': {
+                    'Ref': 'PublicRouteTable'
+                }
+            }
+        }
 
     def create_route_tables(self, public_rtb=None, private_rtb=None, protected_rtb=None):
         if public_rtb:
+            # create public route table
             self.resources['PublicRouteTable'] = {
                 'Type': 'AWS::EC2::RouteTable',
                 'Properties': {
-                    'Tags': [{'Name': public_rtb}],
+                    'Tags': [{'Key': 'Name', 'Value': public_rtb}],
                     'VpcId': {
                         'Ref': 'VPC'
                     }
                 }
             }
+
+            # associate public subnets to public route table
             for subnet_name in self.public_subnet_name:
                 self.resources[subnet_name['cloudformation'] + 'RouteTableAssociation'] = {
                     'Type': 'AWS::EC2::SubnetRouteTableAssociation',
@@ -331,37 +384,133 @@ class CreateYAML:
                 }
 
         if private_rtb:
-            for i, subnet in enumerate(private_subnet):
-                self.resources['PrivateSubnet' + str(i)] = {
-                    'Type': 'AWS::EC2::Subnet',
+            # create private route tables
+            for i, rtb in enumerate(private_rtb):
+                self.resources['PrivateRouteTable' + str(i)] = {
+                    'Type': 'AWS::EC2::RouteTable',
                     'Properties': {
-                        'AvailabilityZone': subnet['az'],
-                        'CidrBlock': subnet['cidr'],
-                        'MapPublicIpOnLaunch': True,
-                        'Tags': [{'Name': subnet['name']}],
+                        'Tags': [{'Key': 'Name', 'Value': rtb['name']}],
                         'VpcId': {
                             'Ref': 'VPC'
                         }
                     }
                 }
-                self.private_subnet_name.append({'cloudformation': 'PrivateSubnet' + str(i), 'name': subnet['name']})
 
-        if protected_subnet:
-            for i, subnet in enumerate(protected_subnet):
-                self.resources['ProtectedSubnet' + str(i)] = {
-                    'Type': 'AWS::EC2::Subnet',
+                # associate each private subnet to each private route table
+                subnet_cloudformation_name = next(
+                    item for item in self.private_subnet_name if item['name'] == rtb['subnet'])
+                self.resources[subnet_cloudformation_name['cloudformation'] + 'RouteTableAssociation'] = {
+                    'Type': 'AWS::EC2::SubnetRouteTableAssociation',
                     'Properties': {
-                        'AvailabilityZone': subnet['az'],
-                        'CidrBlock': subnet['cidr'],
-                        'MapPublicIpOnLaunch': True,
-                        'Tags': [{'Name': subnet['name']}],
-                        'VpcId': {
-                            'Ref': 'VPC'
+                        'SubnetId': {
+                            'Ref': subnet_cloudformation_name['cloudformation']
+                        },
+                        'RouteTableId': {
+                            'Ref': 'PrivateRouteTable' + str(i)
                         }
                     }
                 }
-                self.protected_subnet_name.append(
-                    {'cloudformation': 'ProtectedSubnet' + str(i), 'name': subnet['name']})
+
+                self.private_rtb_name.append(
+                    {'cloudformation': 'PrivateRouteTable' + str(i), 'name': rtb['name']})
+
+        if protected_rtb:
+            # create protected route table
+            self.resources['ProtectRouteTable'] = {
+                'Type': 'AWS::EC2::RouteTable',
+                'Properties': {
+                    'Tags': [{'Key': 'Name', 'Value': protected_rtb}],
+                    'VpcId': {
+                        'Ref': 'VPC'
+                    }
+                }
+            }
+
+            # associate protected subnets to protected route table
+            for subnet_name in self.protected_subnet_name:
+                self.resources[subnet_name['cloudformation'] + 'RouteTableAssociation'] = {
+                    'Type': 'AWS::EC2::SubnetRouteTableAssociation',
+                    'Properties': {
+                        'SubnetId': {
+                            'Ref': subnet_name['cloudformation']
+                        },
+                        'RouteTableId': {
+                            'Ref': 'ProtectRouteTable'
+                        }
+                    }
+                }
+
+    def create_nat(self, nat=None):
+        # create nat
+        for i, _nat in enumerate(nat):
+            # create elastic ip
+            self.resources['EIP' + str(i)] = {
+                'Type': 'AWS::EC2::EIP',
+                'Properties': {
+                    'Tags': [{'Key': 'Name', 'Value': _nat['eip']}],
+                }
+            }
+
+            # create nat gateway
+            self.resources['NAT' + str(i)] = {
+                'Type': 'AWS::EC2::NatGateway',
+                'Properties': {
+                    'AllocationId': {
+                        'GetAtt': 'EIP' + str(i)
+                    },
+                    'SubnetId': {
+                        'Ref': _nat['subnet']
+                    },
+                    'Tags': [{'Key': 'Name', 'Value': _nat['name']}]
+                }
+            }
+
+            # routing
+            rtb_name = next(item for item in self.private_rtb_name if item['name'] == _nat['subnet'])
+            self.resources['{}RouteNAT{}'.format(rtb_name, str(i))] = {
+                'Type': 'AWS::EC2::Route',
+                'Properties': {
+                    'DestinationCidrBlock': '0.0.0.0/0',
+                    'NatGatewayId': {
+                        'Ref': 'NAT' + str(i)
+                    },
+                    'RouteTableId': {
+                        'Ref': rtb_name
+                    }
+                }
+            }
+
+    def create_s3_ep(self, s3_gateway_ep=None):
+        rtb_list = []
+
+        for rtb in s3_gateway_ep['route-table']:
+            rtb_name = next(item for item in self.private_rtb_name if item['name'] == rtb)
+            rtb_list.append({'Ref': rtb_name['cloudformation']})
+
+        self.resources['S3EP'] = {
+            'Type': 'AWS::EC2::VPCEndpoint',
+            'Properties': {
+                'RouteTableIds': rtb_list,
+                'ServiceName': 'com.amazonaws.{}.s3'.format(self.region),
+                'VpcId': {
+                    'Ref': 'VPC'
+                }
+            }
+        }
+
+    def create_yaml(self):
+        template = {
+            'AWSTemplateFormatVersion': '2010-09-09',
+            'Description': 'VPC Stack Generator CLI',
+            'Resources': self.resources
+        }
+
+        try:
+            with open('template.yaml', 'w') as f:
+                yaml.dump(template, f)
+
+        except Exception as e:
+            print(e)
 
 
 class Command:
@@ -374,6 +523,7 @@ class Command:
     public_subnet = []
     private_subnet = []
     protected_subnet = []
+    k8S_tag = False
     igw = None
     eip = []
     nat = []
@@ -450,6 +600,10 @@ class Command:
         self.set_private_subnet()
         self.set_protected_subnet()
 
+        # skip creating k8s tags weh public and private subnet hasn't nothing
+        if len(self.public_subnet) or len(self.private_subnet):
+            self.set_subnet_k8s_tags()
+
         # skip creating igw when public subnet hasn't nothing
         if len(self.public_subnet):
             self.set_internet_gateway()
@@ -489,6 +643,22 @@ class Command:
 
         # print tables
         self.print_tables()
+
+        # create template yaml file
+        yaml_file = CreateYAML(
+            region=self.region,
+            vpc=self.vpc,
+            public_subnet=self.public_subnet,
+            private_subnet=self.private_subnet,
+            protected_subnet=self.protected_subnet,
+            k8s_tags=self.k8S_tag,
+            igw=self.igw,
+            public_rtb=self.public_rtb,
+            private_rtb=self.private_rtb,
+            protected_rtb=self.protected_rtb,
+            s3_gateway_ep=self.s3_gateway_ep
+        )
+        yaml_file.create_yaml()
 
     # print figlet
     def print_figlet(self):
@@ -708,6 +878,19 @@ class Command:
         else:  # not create protected subnets
             return None
 
+    def set_subnet_k8s_tags(self):
+        questions = [
+            {
+                'type': 'confirm',
+                'name': 'k8s-tag',
+                'message': 'Do you want to create tags for Kubernetes?',
+                'default': False
+            }
+        ]
+
+        answer = prompt(questions=questions)
+        self.k8S_tag = answer['k8s-tag']
+
     def set_internet_gateway(self):
         questions = [
             {
@@ -750,14 +933,14 @@ class Command:
                     'message': 'NAT Gateway {} subnet:'.format(i + 1),
                     'choices': ['{} ({} {})'.format(d['name'], d['cidr'], d['az']) for d in self.public_subnet],
                     'filter': lambda val: re.sub(pattern=r'\([^)]*\)', repl='', string=val).strip(),
-                    'default': i
+                    'default': i + 1
                 },
                 {
                     'type': 'list',
                     'name': 'eip',
                     'message': 'NAT Gateway {} elastic ip:'.format(i + 1),
                     'choices': self.eip,
-                    'default': i
+                    'default': i + 1
                 }
             ]
 
