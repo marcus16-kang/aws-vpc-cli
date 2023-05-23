5afc3fc6bbe6d957eb944004a7e645b73b7d556c
marcus16-kang <marcus16-kang@outlook.com>
2023-05-23 16:36:22 +0900

    Bump dependencies.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 requires = [
     'ansicon>=1.89.0',
-    'aws-cloudformation-visualizer>=0.0.2',
+    'aws-cloudformation-visualizer>=0.0.3',
     'blessed>=1.20.0',
     'boto3>=1.26.81',
     'botocore>=1.29.81',
@@ -15,7 +15,7 @@ requires = [
     'python-dateutil>=2.8.2',
     'python-editor>=1.0.4',
     'PyYAML>=6.0',
-    'readchar==4.0.3',
+    'readchar==4.0.5',
     's3transfer>=0.6.0',
     'six>=1.16.0',
     'urllib3>=1.26.14',
@@ -24,7 +24,7 @@ requires = [
 
 setup(
     name='aws-vpc-cli',
-    version='0.5.6',
+    version='0.5.7',
     author='marcus16-kang',
     description='AWS VPC CloudFormation Stack Generator',
     author_email='marcus16-kang@outlook.com',
